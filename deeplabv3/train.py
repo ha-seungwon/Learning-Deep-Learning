17169a44f29984433df269f6d933148363529177
ha-seungwon <12gktmddnjs@naver.com>
2023-07-10 21:06:24 +0900

    feat: 07.10 code fix

--- deeplabv3/train.py
+++ deeplabv3/train.py
@@ -6,6 +6,7 @@ import deeplabv3
 import pandas as pd
 from PIL import Image
 from torch.utils.data import Dataset
+import dataloader
 
 # Define the loss function
 criterion = nn.CrossEntropyLoss()
@@ -39,6 +40,8 @@ def train_model(model, dataloader, criterion, optimizer, device, num_epochs):
 
             # Forward pass
             outputs = model(inputs)
+            print("outputs",outputs.size())
+            print("labels",labels.size())
             loss = criterion(outputs, labels)
 
             # Backward pass and optimization
@@ -55,59 +58,7 @@ def train_model(model, dataloader, criterion, optimizer, device, num_epochs):
         print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {epoch_loss:.4f}")
 
 
-class ImageSegmentationDataset(Dataset):
-    def __init__(self, image_paths, mask_paths, transform=None):
-        self.image_paths = image_paths
-        self.mask_paths = mask_paths
-        self.transform = transform
 
-    def __len__(self):
-        return len(self.image_paths)
-
-    def __getitem__(self, idx):
-        image_path = self.image_paths[idx]
-        mask_path = self.mask_paths[idx]
-
-        # Load the image and mask using PIL
-        image = Image.open(image_path).convert("RGB")
-        mask = Image.open(mask_path).convert("L")  # Convert to grayscale
-
-        # Apply transformations to the image
-        if self.transform is not None:
-            image = self.transform(image)
-
-        # Apply transformations to the mask without normalization
-        if self.transform is not None:
-            mask_transform = Compose([
-                ToTensor()  # Converts PIL image to torch.Tensor
-            ])
-            mask = mask_transform(mask)
-
-        return image, mask.squeeze(0).long()
-
-# Load the CSV file containing image and mask paths
-df = pd.read_csv('./archive/meta_data.csv')
-image_path = df['image'].tolist()
-mask_path = df['mask'].tolist()
-
-# Prepend the directory path to the file names
-image_dir = 'archive/Forest Segmented/Forest Segmented/images/'
-mask_dir = 'archive/Forest Segmented/Forest Segmented/masks/'
-image_path = [image_dir + path for path in image_path]
-mask_path = [mask_dir + path for path in mask_path]
-
-transform = Compose([
-    ToTensor(),  # Converts PIL image to torch.Tensor
-    Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize image
-])
-
-# Create an instance of the dataset
-dataset = ImageSegmentationDataset(image_path, mask_path, transform=transform)
-
-# Create a data loader for training
-batch_size = 16
-shuffle = True
-train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
-
+dataset=dataloader.CocoDataset("/Users/haseung-won/Desktop/학교/연구실/data/coco_minitrain_25k")
 num_epochs = 10
-train_model(model, train_dataloader, criterion, optimizer, device, num_epochs)
+train_model(model, dataset, criterion, optimizer, device, num_epochs)
