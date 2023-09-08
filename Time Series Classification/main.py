f1639ada351cef8b257bae934e6888ce5497d8ba
unknown <12gktmddnjs@naver.com>
2023-09-08 22:10:15 +0900

    feat: time series init

--- Time Series Classification/main.py
+++ Time Series Classification/main.py
@@ -19,86 +19,67 @@ def seed_everything(seed):
 
 seed_everything(42)  # Seed 고정
 
-df_list=[]
-for subject_id in range(1,10,1):
-
-    dir_csv = f'../PAMAP2_Dataset/Protocol_csv/subject10{str(subject_id)}.csv'
-
-    # Define column names based on the provided structure
-    column_names = [
-        'timestamp',
-        'activityID',
-        'heart_rate',
-    ]
-
-    imu_sensors = [
-        'temperature',
-        'acceleration16g_x', 'acceleration16g_y', 'acceleration16g_z',
-        'acceleration6g_x', 'acceleration6g_y', 'acceleration6g_z',
-        'gyroscope_x', 'gyroscope_y', 'gyroscope_z',
-        'magnetometer_x', 'magnetometer_y', 'magnetometer_z',
-        'orientation_1', 'orientation_2', 'orientation_3','orientation_4'
-    ]
-
-    imu_parts = ['hand', 'chest', 'ankle']
-
-    # Add column names for IMU hand, chest, and ankle data
-    for part in imu_parts:
-        for sensor in imu_sensors:
-            column_names.append(f'IMU_{part}_{sensor}')
-    # Read the CSV file using pandas
-    df = pd.read_csv(dir_csv, names=column_names)
-    df_list.append(df)
-
-    # Now df contains the data from the CSV file with appropriate column names
-
-
-# 데이터 병합
-data = pd.concat(df_list, ignore_index=True)
-
-subject_ids = []  # Subject ID 값을 저장할 빈 리스트 생성
-
-for subject_id in range(1, 10, 1):
-    subject_ids += [subject_id] * len(df_list[subject_id - 1])  # Subject ID 반복하여 리스트에 추가
-
-data['subjectID'] = subject_ids  # Subject ID 컬럼을 데이터프레임에 추가
-
+data=pd.read_csv('../PAMAP2_Dataset/train.csv')
+test_data=pd.read_csv('../PAMAP2_Dataset/test.csv')
 
 data = data.fillna(0)
+test_data = test_data.fillna(0)
 X = data.drop(columns=['activityID'])
 y = data['activityID']
+
+test_X= test_data.drop(columns=['activityID'])
+test_y= test_data['activityID']
+
 num_classes =25
 
+timesteps = 200
+step_size = 20
 
-# 시퀀스 길이 설정
-timesteps = 10
-
-X_train= []
+X_train = []
 y_train = []
 
-for i in range(len(X) - timesteps + 1):
+X_test = []
+y_test = []
+
+# 좌우로 10개의 타임스텝을 겹치게 하려면 step_size를 10으로 설정
+
+for i in range(0, len(X) - timesteps + 1, step_size):
     X_sequence = X.iloc[i:i + timesteps, :].values
-    y_label = y.iloc[i + timesteps - 1]  # Get a single label for the sequence
+    y_label = y.iloc[i + timesteps - 1]  # 시퀀스의 마지막 레이블 가져오기
     X_train.append(X_sequence)
     y_train.append(y_label)
 
+valid_index_range = len(test_X) - timesteps + 1
 
-
+for i in range(0, valid_index_range, step_size):
+    X_sequence = test_X.iloc[i:i + timesteps, :].values
+    y_label = test_y.iloc[i + timesteps - 1]  # 시퀀스의 마지막 레이블 가져오기
+    X_test.append(X_sequence)
+    y_test.append(y_label)
 
 
 X_train_lstm = np.array(X_train)
 y_train_lstm = np.array(y_train)
+
+X_test_lstm = np.array(X_test)
+y_test_lstm = np.array(y_test)
+
+
 # Now you can proceed with multi-label encoding
 y_train_encoded = np.array([to_categorical(labels, num_classes=num_classes) for labels in y_train])
 y_train_encoded_flattened = y_train_encoded.reshape(-1, num_classes)
 
-
+y_test_encoded = np.array([to_categorical(labels, num_classes=num_classes) for labels in y_test])
+y_test_encoded_flattened = y_test_encoded.reshape(-1, num_classes)
 
 # Assuming X_train_lstm, y_train_encoded_flattened are already prepared
 # Convert numpy arrays to PyTorch tensors
 X_train_tensor = torch.tensor(X_train, dtype=torch.float32)
 y_train_encoded_flattened_tensor = torch.tensor(y_train_encoded_flattened, dtype=torch.float32)
 
+
+X_test_tensor = torch.tensor(X_test, dtype=torch.float32)
+y_test_encoded_flattened_tensor = torch.tensor(y_test_encoded_flattened, dtype=torch.float32)
 # Create a custom dataset
 class CustomDataset(Dataset):
     def __init__(self, X, y, edge_index):
@@ -130,47 +111,66 @@ edge_index = torch.tensor(np.array(np.where(adjacency_matrix == 1)), dtype=torch
 # Create instances of custom dataset
 custom_dataset = CustomDataset(X_train_tensor, y_train_encoded_flattened_tensor,edge_index)
 
-# Split the data into training and validation sets
-train_size = int(0.8 * len(custom_dataset))
-val_size = len(custom_dataset) - train_size
-train_dataset, val_dataset = torch.utils.data.random_split(custom_dataset, [train_size, val_size])
 
 # Create data loaders
 batch_size = 64
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+train_loader = DataLoader(custom_dataset, batch_size=batch_size, shuffle=True)
 
-# Define the LSTM model
+# Assuming you have a validation dataset named 'val_dataset'
+val_dataset = CustomDataset(X_test_tensor, y_test_encoded_flattened_tensor, edge_index)  # X_val_tensor, y_val_encoded_flattened_tensor, edge_index_val은 검증 데이터에 대한 텐서와 엣지 인덱스입니다.
+val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
 
 # Initialize model and hyperparameters
 input_size = X_train_tensor.shape[2]
-hidden_size = 50
+hidden_size = 30
 num_classes = 25
 
 model_name=args.model_name
 
+flatten_layer_input=num_classes*timesteps
 if model_name=='LSTM':
     model=models.LSTMModel(input_size, hidden_size, num_classes).to(device)
+    lr = 0.00001
 elif model_name=='Conv1D':
     model=models.Conv1DModel(input_size, num_classes).to(device)
+    lr = 0.001
 elif model_name=='GCN':
-    model=models.GCNModel(input_size,hidden_size, num_classes).to(device)
+    model=models.GCNModel(input_size,hidden_size, num_classes,timesteps).to(device)
     edge_index=custom_dataset.edge_index
     edge_index=edge_index.to(device)
+    lr = 0.0001
 elif model_name=='GCN2':
-    model=models.GCNModel2(input_size,hidden_size, num_classes).to(device)
+    model=models.GCNModel2(input_size,hidden_size, num_classes,timesteps).to(device)
     edge_index=custom_dataset.edge_index
     edge_index=edge_index.to(device)
+    lr = 0.00001
+elif model_name=="AutoConv":
+    lr = 0.0001
+    latent_size = 16  # Adjust as needed
+    model = models.ConvAutoencoder(input_size, latent_size).to(device)
+elif model_name=='Generate':
+    lr = 0.001
+    latent_size = 16  # Adjust as needed
+    model = models.AutoConvWithGenerator(input_size, latent_size,num_classes).to(device)
 
 print("model_name : ",model_name,"input_size : ",input_size,"hidden_size : ",hidden_size,"num_classes : ",num_classes)
 criterion = nn.BCEWithLogitsLoss()  # Binary cross-entropy loss
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+optimizer = optim.Adam(model.parameters(), lr=lr)
 
 from tqdm import tqdm  # Import tqdm library
 
+
+def some_custom_loss(generator_label_output,target):
+    # Define your custom loss here
+    # For example, you can use Mean Squared Error (MSE) loss
+    mse_loss = nn.MSELoss()
+    loss = mse_loss(generator_label_output, target)  # Define 'target' according to your task
+    return loss
+
+
 # Training loop
-num_epochs = 10
+num_epochs = 30
 for epoch in range(num_epochs):
     model.train()
     train_loader_tqdm = tqdm(train_loader, desc=f"Epoch [{epoch + 1}/{num_epochs}] Training", leave=False)
@@ -180,10 +180,38 @@ for epoch in range(num_epochs):
         optimizer.zero_grad()
         if 'GCN' in model_name:
             outputs = model(inputs,edge_index)
+            loss = criterion(outputs, labels)
+        elif model_name=="AutoConv":
+            target = inputs[:, -1, :]  # Use the last time step as the target for autoencoder
+            reconstructed_outputs = model(inputs)
+            # Select the last time step from reconstructed_outputs
+            reconstructed_outputs = reconstructed_outputs[:, -1, :]
+            loss = criterion(reconstructed_outputs, target)
+        elif model_name == "Generate":
+            target = inputs[:, -1, :]  # Use the last time step as the target for autoencoder
+
+            # 모델의 출력 중에서 decoder_outputs와 generator_label_output을 얻습니다.
+            decoder_outputs, generator_label_output = model(inputs)
+
+            # Select the last time step from decoder_outputs
+            decoder_outputs = decoder_outputs[:, -1, :]
+
+            # Autoencoder 부분의 손실 계산 (예: 평균 제곱 오차)
+            label_loss = criterion(generator_label_output, labels)
+
+            # Custom loss 함수인 some_custom_loss를 이용해 추가적인 손실 계산
+            custom_loss = some_custom_loss(decoder_outputs, target)
+
+            # Autoencoder 손실과 추가적인 손실을 합하여 총 손실 계산
+            loss = label_loss + custom_loss
+
         else:
             outputs = model(inputs)
+            loss = criterion(outputs, labels)
 
-        loss = criterion(outputs, labels)
+
+
+
         loss.backward()
         optimizer.step()
 
@@ -197,9 +225,35 @@ for epoch in range(num_epochs):
             inputs, labels = inputs.to(device), labels.to(device)
             if 'GCN' in model_name:
                 outputs = model(inputs, edge_index)
+                loss = criterion(outputs, labels)
+            elif model_name == "AutoConv":
+                target = inputs[:, -1, :]  # Use the last time step as the target for autoencoder
+                reconstructed_outputs = model(inputs)
+                # Select the last time step from reconstructed_outputs
+                reconstructed_outputs = reconstructed_outputs[:, -1, :]
+                loss = criterion(reconstructed_outputs, target)
+            elif model_name == "Generate":
+                target = inputs[:, -1, :]  # Use the last time step as the target for autoencoder
+
+                # 모델의 출력 중에서 decoder_outputs와 generator_label_output을 얻습니다.
+                decoder_outputs, generator_label_output = model(inputs)
+
+                # Select the last time step from decoder_outputs
+                decoder_outputs = decoder_outputs[:, -1, :]
+
+                # Autoencoder 부분의 손실 계산 (예: 평균 제곱 오차)
+                label_loss = criterion(generator_label_output, labels)
+
+                # Custom loss 함수인 some_custom_loss를 이용해 추가적인 손실 계산
+                custom_loss = some_custom_loss(decoder_outputs, target)
+
+                # Autoencoder 손실과 추가적인 손실을 합하여 총 손실 계산
+                loss = label_loss + custom_loss
+                print(f"Label Loss: {label_loss:.4f}, Custom Loss: {custom_loss:.4f}")
+
             else:
                 outputs = model(inputs)
-            loss = criterion(outputs, labels)
+                loss = criterion(outputs, labels)
             val_loss += loss.item()
 
     val_loss /= len(val_loader)
